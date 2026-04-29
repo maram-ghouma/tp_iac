5691cda604d920aff881db3b760915ae290d1a4e
unknown <ghoumamaram@gmail.com>
2026-04-29 17:42:56 +0100

    changing

--- main.tf
+++ main.tf
@@ -1,33 +1,32 @@
-# --- Terraform Configuration & Provider ---
+# --- Configuration Terraform et Provider ---
+
 terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0.1"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "docker" {}
 
-# --- Resource 1: PostgreSQL Image ---
-# Tells Terraform to pull this image from Docker Hub
+# --- 1. Ressource : Base de Données PostgreSQL ---
+
 resource "docker_image" "postgres_image" {
   name         = "postgres:latest"
-  keep_locally = true  # Don't delete the image on terraform destroy
+  keep_locally = true
 }
 
-# --- Resource 2: PostgreSQL Container ---
 resource "docker_container" "db_container" {
   name  = "tp-db-postgres"
   image = docker_image.postgres_image.image_id
 
   ports {
-  internal = 5432
-  external = var.db_port_external
+    internal = 5432
+    external = var.db_external_port
   }
 
-  # Injecting our variables.tf values as environment variables
   env = [
     "POSTGRES_USER=${var.db_user}",
     "POSTGRES_PASSWORD=${var.db_password}",
@@ -35,23 +34,34 @@ resource "docker_container" "db_container" {
   ]
 }
 
-# --- Resource 3: Web App Image ---
-# Terraform will run docker build using our Dockerfile_app
-resource "docker_image" "app_image" {
-  name         = "tp-web-app:latest"
-  keep_locally = true
+# --- 2. Ressource : Application Web Nginx ---
+
+# Workaround: the provider's build block fails on Windows (legacy build API bug).
+# We call Docker directly via local-exec, which works identically to the manual command.
+resource "null_resource" "build_app" {
+  triggers = {
+    dockerfile_hash = filemd5("${path.module}/Dockerfile_app")
+  }
+
+  provisioner "local-exec" {
+    command     = "docker build -t tp-web-app:latest -f Dockerfile_app ."
+    working_dir = path.module
+  }
 }
 
-# --- Resource 4: Web App Container ---
+resource "docker_image" "app_image" {
+  name       = "tp-web-app:latest"
+  depends_on = [null_resource.build_app]
+}
+
 resource "docker_container" "app_container" {
   name  = "tp-app-web"
   image = docker_image.app_image.image_id
 
-  # This tells Terraform: create the DB container BEFORE this one
   depends_on = [docker_container.db_container]
 
   ports {
     internal = 80
-    external = var.app_port_external  # References our variable → 8080
+    external = var.app_port_external
   }
 }
